a75ac6616398235d3d534e39114e5661b43dead8
ZaBralow <pavlik.elsukov@mail.ru>
2019-01-28 10:57:53 +0300

    spoiler

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-$(document)	
+$(function () {
+	$('.spoiler-body').hide(300);
+	$(document).on('click','.spoiler-head',function (e) {
+		e.preventDefault()
+		$(this).parents('.spoiler-wrap').toggleClass("active").find('.spoiler-body').slideToggle();
+	})
+})
